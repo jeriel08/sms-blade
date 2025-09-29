80f2042c29bc7e739261a4441361cb34ea44612b
Jeriel Sanao <sanaojeriellian@gmail.com>
2025-09-29 22:42:45 +0800

    Integrate hugeicons for icons

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,10 +12,8 @@
 </div>
     <!-- Navigation Links - This will grow to take available space -->
     <div class="flex-1 px-4 py-4 space-y-2">
-        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="!justify-start !px-4 !py-3">
-            <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-            </svg>
+        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="flex gap-2 !justify-start !px-4 !py-3">
+            <x-hugeicons-dashboard-square-02 />
             {{ __('Dashboard') }}
         </x-nav-link>
         
@@ -44,10 +42,8 @@
             </x-slot>
 
             <x-slot name="content">
-                <x-dropdown-link :href="route('profile.edit')" class="flex items-center">
-                    <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                    </svg>
+                <x-dropdown-link :href="route('profile.edit')" class="flex items-center gap-2">
+                    <x-hugeicons-user-account />
                     {{ __('Profile') }}
                 </x-dropdown-link>
 
@@ -57,12 +53,10 @@
                     <x-dropdown-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();"
-                            class="flex items-center text-red-600 hover:text-red-700">
-                        <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                        </svg>
+                            class="flex items-center gap-2 text-red-600 hover:text-red-700">
+                        <x-hugeicons-logout-02 />
                         {{ __('Log Out') }}
-                    </x-dropdown-link>
+                    </x->
                 </form>
             </x-slot>
         </x-dropdown>
