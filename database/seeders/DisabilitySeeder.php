5eb38866a3f9965785bcf50ca4881dd285d1291d
Jeriel Sanao <sanaojeriellian@gmail.com>
2025-10-07 12:31:09 +0800

    Add more data on DisabilitySeeder

--- database/seeders/DisabilitySeeder.php
+++ database/seeders/DisabilitySeeder.php
@@ -13,7 +13,17 @@ class DisabilitySeeder extends Seeder
      */
     public function run(): void
     {
-        Disability::create(['disability_id' => 1, 'name' => 'Visual Impairment']);
-        Disability::create(['disability_id' => 2, 'name' => 'Hearing Impairment']);
+        Disability::create(['disability_id' => 1, 'name' => 'Visual Impairment (Blind)']);
+        Disability::create(['disability_id' => 2, 'name' => 'Visual Impairment (Low Vision)']);
+        Disability::create(['disability_id' => 3, 'name' => 'Hearing Impairment']);
+        Disability::create(['disability_id' => 4, 'name' => 'Autism Spectrum Disorder']);
+        Disability::create(['disability_id' => 5, 'name' => 'Multiple Disorder']);
+        Disability::create(['disability_id' => 6, 'name' => 'Speech/Language Disorder']);
+        Disability::create(['disability_id' => 7, 'name' => 'Learning Disability']);
+        Disability::create(['disability_id' => 8, 'name' => 'Emotional-Behavior Disorder']);
+        Disability::create(['disability_id' => 9, 'name' => 'Cerebral Palsy']);
+        Disability::create(['disability_id' => 10, 'name' => 'Intellectual Disability']);
+        Disability::create(['disability_id' => 11, 'name' => 'Orthopedic/Physical Handicap']);
+        Disability::create(['disability_id' => 12, 'name' => 'Special Health Problem / Chronic Disease']);
     }
 }
